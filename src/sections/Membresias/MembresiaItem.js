d1a2bf68bbc523a0ed088892faa85de2428c845b
Cristian González <kazragore96@gmail.com>
2021-09-09 18:10:53 -0500

    Agregado animacion de flipcard a las membresias

--- src/sections/Membresias/MembresiaItem.js
+++ src/sections/Membresias/MembresiaItem.js
@@ -1,55 +1,18 @@
 // styles
-import { motion } from 'framer-motion'
-import { useState } from 'react'
-import { sleep } from 'src/utils/sleep'
 import styles from './membresias.module.scss'
 
 const MembresiaItem = ({ src, label, content }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [isContent, setIsContent] = useState(false)
-
-  const onToggle = () => setIsOpen((v) => !v)
-  const initAnimation = () => setIsContent((v) => !v)
-
-  const handleFlip = async () => {
-    initAnimation()
-    await sleep(700)
-    onToggle()
-  }
-
-  const Iconside = () => (
-    <div className={styles.grid_item}>
-      <img src={src} alt="" />
-      <span>{label}</span>
-    </div>
-  )
-
-  const ContentSide = () => (
-    <div className={`${styles.grid_item} ${isOpen ? styles.grid_content : ''}`}>
-      <p>{content}</p>
-    </div>
-  )
-
   return (
-    <div>
-      <motion.div
-        variants={{
-          open: {
-            rotateY: 180,
-            transition: { ease: 'easeInOut', duration: 1.5 }
-          },
-          close: {
-            rotateY: 0,
-            transition: { ease: 'easeInOut', duration: 1.5 }
-          }
-        }}
-        animate={isContent ? 'open' : 'close'}
-      >
-        {isOpen ? ContentSide() : Iconside()}
-      </motion.div>
-      <button className={styles.btn_flip} onClick={handleFlip}>
-        Ver mas
-      </button>
+    <div className={styles.grid_card}>
+      <div className={styles.grid_card_inner}>
+        <div className={styles.grid_card_back}>
+          <p>{content}</p>
+        </div>
+        <div className={styles.grid_card_front}>
+          <img src={src} alt="" />
+          <span>{label}</span>
+        </div>
+      </div>
     </div>
   )
 }
